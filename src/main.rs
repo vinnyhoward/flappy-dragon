6070665e3cc2cbdbe704f0151a004bb28dac84df
Vince Howard <vincenguyenhoward@gmail.com>
2022-01-02 15:14:26 -0700

    added bare minimum logic to start game and render obstacles, jump, and collide with obstacles

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use bracket_lib::prelude::*;
+mod obstacles;
+mod player;
 
+use bracket_lib::prelude::*;
+use obstacles::*;
+use player::*;
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
@@ -14,24 +18,61 @@ impl GameState for State {
 enum GameMode {
     Menu,
     Playing,
-    End
+    End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct State {
-    mode: GameMode
+    player: Player,
+    mode: GameMode,
+    frame_time: f32,
+    obstacle: Obstacle,
+    score: i32,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
-    fn play(&mut self, _ctx: &mut BTerm) {
-        // TODO: Fill in this stub later
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(2, 2, "Press SPACE to flap.");
+        ctx.print(2, 4, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score)
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+            self.mode = GameMode::End;
+        }
     }
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -44,7 +85,7 @@ impl State {
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _ => {},
+                _ => {}
             }
         }
     }
@@ -58,7 +99,7 @@ impl State {
             match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _ => {},
+                _ => {}
             }
         }
     }
